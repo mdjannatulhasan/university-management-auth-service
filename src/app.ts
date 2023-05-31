c03e8e75c75c7625c80d278b21040bf5e3ba68d2
Shah Muhammad Jannatul Hasan <shahmdjhm@gmail.com>
2023-05-31 20:25:30 +0600

    Testing

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
-import express, { Application } from "express";
-import cors from "cors";
+import express, { Application } from 'express'
+import cors from 'cors'
 
-const app: Application = express();
+const app: Application = express()
 
-app.use(cors());
+app.use(cors())
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req, res) => {
-	res.send("Hello World");
-});
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
 
-export default app;
+export default app
